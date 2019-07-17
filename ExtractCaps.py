9630cced8aff25f92fe32dcb622c2d4648ad5432
Bryan <bryan.dov.bergman@gmail.com>
2019-07-17 22:14:53 +0300

    Initial commit

---
+++ ExtractCaps.py
@@ -0,0 +1,31 @@
+_author_ = 'dev'
+
+# for char in number:
+#     if char in '0123456789':
+#         cleanedNumber = cleanedNumber + char
+
+quote = """
+Alright, but apart from the Sanitation, the Medicine, Education, Wine,
+Public Order, Irrigation, Roads, the Fresh-Water System,
+and Public Health, what have the Romans ever done for us?
+"""
+
+# capsLetters = ''
+#
+# for char in quote:
+#     if char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+#         capsLetters = capsLetters + char
+#
+# print(capsLetters)
+
+
+
+for i in range(0,101, 7):
+    print(str(i), end='\t')
+    if i % 7 == 0:
+            print (i)
+
+
+            
+
+
